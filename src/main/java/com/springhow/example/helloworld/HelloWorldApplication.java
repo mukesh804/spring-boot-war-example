26f86b2d0c97c9182dce0cada4504be5b06dff1e
mukesh804 <73191137+mukesh804@users.noreply.github.com>
2021-11-17 00:24:09 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Successfully  Deployed Java Application  On TOMCAT Server 5";
+        return "Successfully  Deployed Java Application  On TOMCAT Server 7";
     }
 
 }
